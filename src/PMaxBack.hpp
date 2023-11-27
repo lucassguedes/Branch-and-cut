5438c49b3e90e69857757604c55154b668eb5318
Lucas Guedes <lucasguedes778@gmail.com>
2023-11-27 20:22:08 -0300

    Insert external code to test.

---
+++ src/PMaxBack.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <map>
+
+typedef vector<int> super_node;
+
+using namespace std;
+
+
+pair<int,double> chooseBestMaxBack(vector<int> *s, int n, double **x, vector<bool>& inS);
+double calculatInitialMaxBackCutMin(int node, int n, double **x);
+void updateBool(vector<bool>&chosed, vector<int> Smin);
+vector<vector<int>> PMaxBack(double **x, int n);
+double sumSuperNodeMaxBack(int firstNode, int secondNode, vector<super_node> *superNodes, double **x);
+pair<int,double> chooseBestMaxBackSuperNodes(vector<int> *s, vector<super_node> *superNodes, double **x);
