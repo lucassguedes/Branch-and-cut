b8d4fab765cc2fdba8c93e5b2b1f0a7585ae6068
Lucas Guedes <lucasguedes778@gmail.com>
2023-12-25 22:52:44 -0300

    Test.

--- test/tester.py
+++ test/tester.py
@@ -3,27 +3,29 @@ import subprocess
 
 
 def main():
+    instances_folder = "../instances/under_300/"
+    output_filename = "under_300.txt"
 
-    result = subprocess.run(["ls", "../instances/"], capture_output=True, text=True)
+    result = subprocess.run(["ls", instances_folder], capture_output=True, text=True)
     instance_names = result.stdout.split("\n")
     instance_names.pop()
 
     output = ""
-    counter = 0;
+    counter = 1
     n_instances = len(instance_names)
     for instance_name in instance_names:
+        print("\033[H")
         print(f"Executing \"{instance_name}\" instance...")
         print(f"Total progress: {counter}/{n_instances}") 
-        result = subprocess.run(["../bc", f"../instances/{instance_name}"], capture_output=True, text=True)
+        result = subprocess.run(["../bc", f"{instances_folder}{instance_name}"], capture_output=True, text=True)
 
         separated_result = result.stdout.split("\n")
         objValue = separated_result[0]
         time = separated_result[1]
         output+=instance_name + "\n\tobj: " + objValue + "\n\ttime: " + time + "\n" 
-        print("\033[H")
         counter+=1
 
-    with open("test_result.txt", "w") as file:
+    with open(output_filename, "w") as file:
         file.write(output)
         
 
